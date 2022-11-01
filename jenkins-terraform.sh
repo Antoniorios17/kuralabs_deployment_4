e62338b8c27ae1b6da8bbd6579479cc0c83b5ce3
AntonioRios <63029609+Antoniorios17@users.noreply.github.com>
2022-10-31 20:58:26 -0400

    Create jenkins-terraform.sh

---
+++ jenkins-terraform.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#Installation of jenkins and its dependencies
+
+sudo apt update
+sudo apt -y install openjdk-11-jre
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
+/usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+/etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt install python3.10-venv -y
+sudo apt-get update
+sudo apt-get -y install jenkins
+sudo systemctl start jenkins
+systemctl status jenkins >> ~/file.txt
+
+#Installation of terraform and its dependencies
+
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install terraform
+sudo apt upgrade -y
